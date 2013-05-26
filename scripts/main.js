c50abf93f217f0654f63df8d8d49e59ef7a5badd
Wesley Sun <sunwes@gmail.com>
2013-05-26 02:59:31 -0500

    added sound and flicker effect

---
+++ scripts/main.js
@@ -0,0 +1,20 @@
+function toggleImage() {
+	var image = $("#dr-racket-img");
+	if (image.attr("src") == "/findler.jpg")
+		image.attr("src","/findler-mask.jpg");
+	else
+		image.attr("src","/findler.jpg");
+}
+state = 1;
+function flickerImage() {
+	toggleImage();
+	state++;
+	if (state < 50) {
+		setTimeout(flickerImage, 1500*Math.pow(0.8, state));
+		console.log(state);
+	}
+}
+$().ready(function(){
+	$("#audio").get(0).play();
+	setTimeout(flickerImage, 1500);
+});
