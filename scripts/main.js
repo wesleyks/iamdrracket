a795af040aeeb114be3e3f7d6cb101ca7cac2fdb
Wesley Sun <sunwes@gmail.com>
2013-05-28 15:09:04 -0500

    made the mask start flickering earlier

--- scripts/main.js
+++ scripts/main.js
@@ -10,11 +10,11 @@ function flickerImage() {
 	toggleImage();
 	state++;
 	if (state < 50) {
-		setTimeout(flickerImage, 1500*Math.pow(0.8, state));
+		setTimeout(flickerImage, 1000*Math.pow(0.8, state));
 		console.log(state);
 	}
 }
 $().ready(function(){
 	$("#audio").get(0).play();
-	setTimeout(flickerImage, 1500);
+	setTimeout(flickerImage, 1000);
 });
